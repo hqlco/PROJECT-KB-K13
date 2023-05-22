a3f6304d9233b0ecce4ab582431f6f0309a01c4e
haqqy <haqqyaminy@gmail.com>
2023-05-23 00:24:56 +0700

    rollback

--- sudoku.py
+++ sudoku.py
@@ -359,65 +359,7 @@ def check_sudoku(sudoku):
     return True
 
 
-N = 9
-
-
-def isSafe(grid, row, col, num):
-
-    for x in range(9):
-        if grid[row][x] == num:
-            return False
-
-    for x in range(9):
-        if grid[x][col] == num:
-            return False
-
-    startRow = row - row % 3
-    startCol = col - col % 3
-    for i in range(3):
-        for j in range(3):
-            if grid[i + startRow][j + startCol] == num:
-                return False
-    return True
-
-
-def solveSudoku(grid, row, col):
-
-    if (row == N - 1 and col == N):
-        return True
-
-    if col == N:
-        row += 1
-        col = 0
-
-    if grid[row][col] > 0:
-        return solveSudoku(grid, row, col + 1)
-    for num in range(1, N + 1, 1):
-
-        if isSafe(grid, row, col, num):
-
-            grid[row][col] = num
-
-            if solveSudoku(grid, row, col + 1):
-                return True
-
-        grid[row][col] = 0
-    return False
-
-
-def hint(game, len_p, len_r, len_c, data, area):
-    data2 = [
-            [0, 0, 0, 9, 0, 0, 0, 3, 0],
-            [3, 0, 6, 0, 2, 0, 0, 4, 0],
-            [2, 0, 4, 0, 0, 3, 1, 0, 6],
-            [0, 7, 0, 0, 5, 1, 0, 8, 0],
-            [0, 3, 1, 0, 6, 0, 0, 5, 7],
-            [5, 0, 9, 0, 0, 0, 6, 0, 0],
-            [4, 1, 0, 0, 0, 2, 0, 7, 8],
-            [7, 6, 3, 0, 0, 5, 4, 0, 0],
-            [9, 2, 8, 0, 0, 4, 0, 0, 1]
-    ]
-    solveSudoku(data2, 0, 0)
+def hint(game, len_p, len_r, len_c):
     if len_p > 1:
         p = random.randrange(0, len_p-1)
     else:
@@ -425,7 +367,8 @@ def hint(game, len_p, len_r, len_c, data, area):
     r = random.randrange(0, len_r-1)
     c = random.randrange(0, len_c-1)
     val = random.randrange(1, 9)
-    game.board[p][r][c].value = data2[r][c]
+
+    game.board[p][r][c].value = val
 
 
 def play():
@@ -736,8 +679,7 @@ def play():
                     return
 
                 if hint_btn.collidepoint(mouse_pos):
-                    hint(game, len(data), len(data[0]), len(
-                        data[0][0]), data, area)
+                    hint(game, len(data), len(data[0]), len(data[0][0]))
 
                 active_cell = None
                 for p in cells:
